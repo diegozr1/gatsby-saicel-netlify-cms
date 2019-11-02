b9d1a8099650e06f25cdaa6fadebf9f26f554910
Diego Zamora <zdr_00@live.com>
2019-11-02 16:51:46 -0600

    changes to patologies design

--- src/pages/patologies/index.js
+++ src/pages/patologies/index.js
@@ -2,39 +2,13 @@ import React from 'react'
 
 import Layout from '../../components/Layout'
 import PatologiesRoll from '../../components/PatologiesRoll'
+import {MainSection} from "../../sections/PatologyPage"
 
 export default class PatologiesIndexPage extends React.Component {
   render() {
-    return (
-      <Layout path={this.props.path}>
-        <div
-          className="full-width-image-container margin-top-0"
-          style={{
-            backgroundImage: `url('/img/patologies-index.jpg')`,
-          }}
-        >
-          <h1
-            className="has-text-weight-bold is-size-1"
-            style={{
-              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#f40',
-              color: 'white',
-              padding: '1rem',
-            }}
-          >
-            Patologías
-          </h1>
-        </div>
-        <p 
-        style={{
-            paddingTop: '4em',
-            paddingRight: '12em',
-            paddingLeft: '4em',
-            fontSize: '35px'
-        }}
-        >
-          Algunos tratamientos en los que hemos estado involucrados con muy buenos resultados son en transtornos neurológicos como: 
-        </p>
+    return (            
+      <Layout path={this.props.path}>                        
+        <MainSection />
         <section className="section">
           <div className="container">
             <div className="content">
@@ -42,7 +16,7 @@ export default class PatologiesIndexPage extends React.Component {
             </div>
           </div>
         </section>
-      </Layout>
+      </Layout>      
     )
   }
 }
